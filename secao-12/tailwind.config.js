e72fb50dcc070479ee59811928ecf7777c993fd8
Michel França <fr.mich17@gmail.com>
2021-10-02 19:19:53 -0300

    Seção 12 concluída

---
+++ secao-12/tailwind.config.js
@@ -0,0 +1,13 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
